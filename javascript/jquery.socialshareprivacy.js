c7dde9b6851c05624aefd3d280e41ef4a877afb0
Rainer Müller <rainer@websimple.be>
2013-06-10 14:00:25 +0200

    google plus pas utilisé

--- javascript/jquery.socialshareprivacy.js
+++ javascript/jquery.socialshareprivacy.js
@@ -108,7 +108,7 @@
                     'language'          : 'en'
                 },
                 'gplus' : {
-                    'status'            : 'on',
+                    'status'            : 'off',
                     'dummy_img'         : 'plugins/citizensartdays/javascript/socialshareprivacy/images/dummy_gplus.png',
                     'txt_info'          : '2 Klicks f&uuml;r mehr Datenschutz: Erst wenn Sie hier klicken, wird der Button aktiv und Sie k&ouml;nnen Ihre Empfehlung an Google+ senden. Schon beim Aktivieren werden Daten an Dritte &uuml;bertragen &ndash; siehe <em>i</em>.',
                     'txt_gplus_off'     : 'nicht mit Google+ verbunden',
