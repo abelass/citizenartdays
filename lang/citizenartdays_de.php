4690847a6d76c3ed5cfa48642bd934412d77bd65
Rainer <rainer@websimple.be>
2015-08-11 09:30:17 +0200

    Hide if too many authors

--- lang/citizenartdays_de.php
+++ lang/citizenartdays_de.php
@@ -1,38 +1,41 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+if (!defined('_ECRIRE_INC_VERSION'))
+  return;
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-    //B
-    'bonjour'=>'Liebe(r)',
+  //B
+  'bonjour' => 'Liebe(r)',
 
-	// C
-	'citizenartdays_titre' => 'Citizen Art Days',
-	
-    //E
-    'empfehlen'=>'Empfehlen Sie uns weiter!',
-	
-	// O
-	'ort' => 'Ort:',  
-	
-    //L
-    'lire_la_suite'=>'Mehr...',      
-	
-    //M
-    'mehr_ueber'=>'Mehr über',
-    'merci_de_votre_reservation'=>'Vielen Dank für Ihre Anmeldung zu den Citizen Art Days. Wir freuen uns auf Ihre Teilnahme.
+  // C
+  'citizenartdays_titre' => 'Citizen Art Days',
+
+  //E
+  'empfehlen' => 'Empfehlen Sie uns weiter!',
+
+  // O
+  'ort' => 'Ort:',
+
+  //K
+  'kuenstler' =>'Künstler',
+
+  //L
+  'lire_la_suite' => 'Mehr...',
+
+  //M
+  'mehr_ueber' => 'Mehr über',
+  'merci_de_votre_reservation' => 'Vielen Dank für Ihre Anmeldung zu den Citizen Art Days. Wir freuen uns auf Ihre Teilnahme.
 Sollte ein Workshop mit limitierter Teilnehmerzahl bereits ausgebucht sein, erhalten Sie von uns umgehend eine Nachricht. 
 <br /><br />
 Mit herzlichen Grüßen <br />
 Das CAD Team
 <br /><br />
-Zur Information: Ihre Anmeldung ist verbindlich. Aufgrund der begrenzten Anzahl an Plätzen bei der Teilnahme an einem Workshop, sagen Sie uns bitte rechtzeitig bescheid wenn Sie verhindert sein sollten, damit Nachrücker eine Chance bekommen.',    
-    
-    //T
-    'termin'=>'Termin:', 
-    'termine'=>'Termine:',       
-);
+Zur Information: Ihre Anmeldung ist verbindlich. Aufgrund der begrenzten Anzahl an Plätzen bei der Teilnahme an einem Workshop, sagen Sie uns bitte rechtzeitig bescheid wenn Sie verhindert sein sollten, damit Nachrücker eine Chance bekommen.',
 
+  //T
+  'termin' => 'Termin:',
+  'termine' => 'Termine:',
+);
 ?>
--- lang/citizenartdays_en.php
+++ lang/citizenartdays_en.php
@@ -1,38 +1,41 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
- 
+if (!defined('_ECRIRE_INC_VERSION'))
+  return;
+
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-    //B
-    'bonjour'=>'Dear',
+  //B
+  'bonjour' => 'Dear',
 
-	// C
-	'citizenartdays_titre' => 'Citizen Art Days',
-	
-    //E
-    'empfehlen'=>'Recommend us!',
-	
-	// O
-	'ort' => 'Place:',    
-	
-    //L
-    'lire_la_suite'=>'More',    
-    
-    //M
-    'mehr_ueber'=>'More about',
-    'merci_de_votre_reservation'=>'Vielen Dank für Ihre Anmeldung zu den Citizen Art Days. Wir freuen uns auf Ihre Teilnahme.
+  // C
+  'citizenartdays_titre' => 'Citizen Art Days',
+
+  //E
+  'empfehlen' => 'Recommend us!',
+
+  // O
+  'ort' => 'Place:',
+
+  //K
+  'kuenstler' =>'Artists',
+
+  //L
+  'lire_la_suite' => 'More',
+
+  //M
+  'mehr_ueber' => 'More about',
+  'merci_de_votre_reservation' => 'Vielen Dank für Ihre Anmeldung zu den Citizen Art Days. Wir freuen uns auf Ihre Teilnahme.
 Sollte ein Workshop mit limitierter Teilnehmerzahl bereits ausgebucht sein, erhalten Sie von uns umgehend eine Nachricht. 
 <br /><br />
 Mit herzlichen Grüßen <br />
 Das CAD Team
 <br /><br />
-Zur Information: Ihre Anmeldung ist verbindlich. Aufgrund der begrenzten Anzahl an Plätzen bei der Teilnahme an einem Workshop, sagen Sie uns bitte rechtzeitig bescheid wenn Sie verhindert sein sollten, damit Nachrücker eine Chance bekommen.',  
+Zur Information: Ihre Anmeldung ist verbindlich. Aufgrund der begrenzten Anzahl an Plätzen bei der Teilnahme an einem Workshop, sagen Sie uns bitte rechtzeitig bescheid wenn Sie verhindert sein sollten, damit Nachrücker eine Chance bekommen.',
 
-    //T
-    'termin'=>'Schedule:', 
-    'termine'=>'Schedule:',        
+  //T
+  'termin' => 'Schedule:',
+  'termine' => 'Schedule:',
 );
-
 ?>
