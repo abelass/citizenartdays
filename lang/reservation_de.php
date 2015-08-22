086f9624140261798dedd2402d833c20660ebffe
Rainer <rainer@websimple.be>
2015-08-22 14:41:56 +0200

    nouvelle explication

--- lang/reservation_de.php
+++ lang/reservation_de.php
@@ -8,9 +8,8 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
   //E
 
-  'explication_login' => 'Liebe Besucher_in,
-hatten Sie schon an einer Veranstaltung der Citizen Art Days in der Vergangenheit teilgenommen und sich mir derselben Emailadresse angemeldet?
-Dann loggen Si sich bitte im <a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">Loginbereich ein</a>.',
+  'explication_login' => 'Liebe Besucher_in, hatten Sie schon an einer Veranstaltung der Citizen Art Days in der Vergangenheit teilgenommen und sich mit derselben Emailadresse auf dieser Seite registriert?
+Dann loggen Sie sich bitte im <a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">Loginbereich ein</a>.',
 
   //L
   'label_enregistrer' => 'Ich möchte mich auf dieser Seite registrieren:',
