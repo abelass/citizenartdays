ad87f0759c7c01c8f72bf0e758991fd4689caeed
Rainer <rainer@websimple.be>
2015-08-22 13:38:37 +0200

    Nur das nötige

--- lang/reservation_de.php
+++ lang/reservation_de.php
@@ -6,39 +6,11 @@ if (!defined('_ECRIRE_INC_VERSION'))
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-  //B
-  'bonjour' => 'Liebe(r)',
-
-  // C
-  'citizenartdays_titre' => 'Citizen Art Days',
-
   //E
-  'empfehlen' => 'Empfehlen Sie uns weiter!',
+
   'explication_login' => 'Liebe Besucher_in,
 hatten Sie schon an einer Veranstaltung der Citizen Art Days in der Vergangenheit teilgenommen und sich mir derselben Emailadresse angemeldet?
 Dann loggen Si sich bitte im <a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">Loginbereich ein</a>.',
 
-  // O
-  'ort' => 'Ort:',
-
-  //K
-  'kuenstler' =>'Künstler',
-
-  //L
-  'lire_la_suite' => 'Mehr...',
-
-  //M
-  'mehr_ueber' => 'Mehr über',
-  'merci_de_votre_reservation' => 'Vielen Dank für Ihre Anmeldung zu den Citizen Art Days. Wir freuen uns auf Ihre Teilnahme.
-Sollte ein Workshop mit limitierter Teilnehmerzahl bereits ausgebucht sein, erhalten Sie von uns umgehend eine Nachricht. 
-<br /><br />
-Mit herzlichen Grüßen <br />
-Das CAD Team
-<br /><br />
-Zur Information: Ihre Anmeldung ist verbindlich. Aufgrund der begrenzten Anzahl an Plätzen bei der Teilnahme an einem Workshop, sagen Sie uns bitte rechtzeitig bescheid wenn Sie verhindert sein sollten, damit Nachrücker eine Chance bekommen.',
-
-  //T
-  'termin' => 'Termin:',
-  'termine' => 'Termine:',
 );
 ?>
