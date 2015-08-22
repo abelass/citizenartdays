ed0b69a4c8d5b823ce454e88d2d51a3517fd3527
Rainer <rainer@websimple.be>
2015-08-22 13:42:53 +0200

    Bessere Übersetzung

--- lang/reservation_de.php
+++ lang/reservation_de.php
@@ -12,5 +12,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 hatten Sie schon an einer Veranstaltung der Citizen Art Days in der Vergangenheit teilgenommen und sich mir derselben Emailadresse angemeldet?
 Dann loggen Si sich bitte im <a rel="nofollow" class="login_modal" href="@url@" title="@titre_login@">Loginbereich ein</a>.',
 
+  //L
+  'label_enregistrer' => 'Ich möchte mich auf dieser Seite registrieren:',
 );
 ?>
