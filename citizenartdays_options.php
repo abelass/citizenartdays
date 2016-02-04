1b6e8911e0b6f838b9af155c78e1ef7e25059aec
Rainer <rainer@websimple.be>
2016-02-04 12:09:58 +0100

    adding the view port for correct respnive recognizing

--- citizenartdays_options.php
+++ citizenartdays_options.php
@@ -9,16 +9,15 @@
  * @package    SPIP\Citizenartdays\Options
  */
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
+if (!defined('_ECRIRE_INC_VERSION'))
+	return;
 
 //Mettre par défaut le cookie langue en DE
-if(!isset($_COOKIE['spip_lang'])){
-        include_spip('inc/cookie');
-        //Soit un cookie lang est présent
-        spip_setcookie('spip_langue_preferee', 'de', time() + 3660*24*365, chemin_cookie());
-        spip_setcookie('spip_lang', 'de', time() + 3660*24*365, chemin_cookie());        
+if (!isset($_COOKIE['spip_lang'])) {
+	include_spip('inc/cookie');
+	//Soit un cookie lang est présent
+	spip_setcookie('spip_langue_preferee', 'de', time() + 3660 * 24 * 365, chemin_cookie());
+	spip_setcookie('spip_lang', 'de', time() + 3660 * 24 * 365, chemin_cookie());
 
-    }
-
-
+}
 ?>
